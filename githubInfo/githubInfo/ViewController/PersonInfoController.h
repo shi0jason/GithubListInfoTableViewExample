f20a57690cef91998d12e959797d8e99a32f3703
Jason <shi0jason@gmail.com>
2020-03-26 09:38:55 +0800

    add PersonInfoController setup UI & constraint

---
+++ githubInfo/githubInfo/ViewController/PersonInfoController.h
@@ -0,0 +1,19 @@
+//
+//  PersonInfoController.h
+//  githubInfo
+//
+//  Created by cch on 2020/3/24.
+//  Copyright © 2020 cch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PersonInfoController : UIViewController
+
+@property (strong, nonatomic) NSString *infoLink;
+
+@end
+
+NS_ASSUME_NONNULL_END
